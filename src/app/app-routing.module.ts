4abd2c38e96b184827e472b36e6780582fe3691f
Samir BOUHASSOUN <samir.bouhassoun@zenika.com>
2023-02-05 15:27:13 +0100

    Add route for companions-cards component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanionsCardsComponent } from './companions-cards/companions-cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {
+    path: 'companions',
+    component: CompanionsCardsComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
